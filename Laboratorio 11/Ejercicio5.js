52dbaa027c27e9d6a82ff589121a7142705ec40e
Gian Franco Apaza Quispe <gapazaqui@unsa.edu.pe>
2025-10-23 19:34:25 -0500

    Añadiendo ejercicio5.js

---
+++ Laboratorio 11/Ejercicio5.js
@@ -0,0 +1,29 @@
+let billetes100, billetes50, billetes20, billetes10, resto;
+let repetir;
+
+do {
+    repetir = false
+    console.log("--- Cajero Automático ---");
+    let monto = parseInt(prompt("Ingrese el monto a retirar:"));
+    console.log("Monto ingresado: " + monto + " soles");
+    billetes100 = parseInt(monto/100);
+    resto = monto%100;
+    billetes50 = parseInt(resto/50);
+    resto %= 50;
+    billetes20 = parseInt(resto/20);
+    resto %= 20;
+    billetes10 = parseInt(resto/10);
+    console.log("--- Billetes Recibidos ---");
+    console.log("Billetes de 100 soles: " + billetes100
+        +"\nBilletes de 50 soles: " + billetes50
+        +"\nBilletes de 20 soles: " + billetes20
+        +"\nBilletes de 10 soles: " + billetes10
+    );
+    let respuesta = prompt("¿Desea ingresar otro monto? (SÍ / NO)");
+    console.log("Respuesta: " + respuesta);
+    if(respuesta.toLowerCase() === "sí") {
+        repetir = true
+    } else {
+        console.log("Cerrando el sistema...");
+    }
+} while(repetir)   
